683237135d2cfdb0e086fb167c142d04d1903307
Reinaldo L <reinaldovenenusa@gmail.com>
2024-02-18 12:02:58 -0300

    primer

---
+++ src/views/Home.jsx
@@ -0,0 +1,12 @@
+const Home = () => {
+  return (
+    <>
+      <h1 className="mt-5 header">
+        Bienvenido a <b> Happy Cake </b>
+      </h1>
+      <h6 className="subtitle"> El lugar de los pateles felices </h6>
+      <h1 className="display-1 icon"> 🥮 </h1>
+    </>
+  );
+};
+export default Home;
